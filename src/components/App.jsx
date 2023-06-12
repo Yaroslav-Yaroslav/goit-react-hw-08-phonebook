d95eac45ba74c8f9ed0e504104c060ae2b56d566
Yaroslav Yakovenko <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2023-06-12 21:00:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,9 +20,7 @@ export const App = () => {
   return (
     <Layout>
       <h1>Phonebook</h1>
-
       <ContactForm />
-
       <h2>Contacts</h2>
       <Filter />
       {isLoading && !error && <b>Request in progress...</b>}
