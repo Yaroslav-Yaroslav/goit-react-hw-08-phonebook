8f61e71776565af94a6146076934a1fabedab63f
Yaroslav Yakovenko <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2023-09-05 17:10:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <BrowserRouter basename="/goit-react-hw-08-phonebook">
